f40304af675b0a7484d4987b760933912e812abc
Web-UI-Developer <sampreet.github@gmail.com>
2018-07-12 22:39:34 -0400

    Adding loading Animation

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Education-Loan-Calculator
-
-JavaScript 
--- eduLoan.js
+++ eduLoan.js
@@ -1,6 +1,11 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults); 
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+}); 
 
-function calculateResults(e) {                                                     // calculate Results
+function calculateResults() {                                                     // calculate Results
  
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -18,13 +23,16 @@ function calculateResults(e) {                                                  
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
     } else {
     showError ('Please check your Numbers')
    }
-   e.preventDefault();
 } 
 
 function showError(error) {                                                      // Custom Error Function
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
     const errorDiv = document.createElement('div');
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
